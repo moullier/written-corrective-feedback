b732288f1c5fb305208728a786402868fc693a32
Maria Francis-Moullier <moullier@gmail.com>
2020-05-20 22:08:27 -0700

    basic dependencies installed, user model created

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const session = require("express-session");
+const db = require("./models");
+
+
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Requiring our routes
+// require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+
+
+
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
