e7f3815e74572381e730da8f0f870918be062993
Maria Francis-Moullier <moullier@gmail.com>
2020-05-22 18:03:53 -0700

    database interface created, basic login route

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,26 +1,74 @@
 import React, { Component } from "react";
+import Axios from "axios";
 import "../../App.css";
 import "./index.css";
+import $ from "jquery";
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    console.log(event.target.name);
+    
+    this.setState({
+      ...this.state,
+      [event.target.name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("attempt login");
+
+    Axios.post("/api/login", {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then((data) => {
+      console.log("data: ");
+      let firstTime = data.data.ftu;
+      console.log(firstTime);
+      // if user is a first time user, push to FTU page, otherwise to dashboard
+      if(firstTime) {
+        this.props.history.push("/ftu");
+        window.location.reload();
+      } else {
+        this.props.history.push("/dashboard");
+        window.location.reload();
+      }
+    })
+    .catch(function(err) {
+      console.log("Error");
+      console.log(err);
+      $("#li-error").show().html("<strong>Error:</strong> Incorrect email or password");
+      // alert(err.message);
+    });
+  }
 
   render() {
       return (
         <div className="text-center">
-        <form className="form-signin">
+        <form className="form-signin" onSubmit={this.handleSubmit}>
         <img className="mb-4" src="http://getbootstrap.com/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
         <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="inputEmail" className="sr-only">Email address</label>
-        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-        <label for="inputPassword" className="sr-only">Password</label>
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-          </label>
-        </div>
-        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p className="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+        <input type="email" name="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus onChange={this.handleChange}/>
+        <label htmlFor="inputPassword" className="sr-only">Password</label>
+        <input type="password" name="password" id="inputPassword" className="form-control" placeholder="Password" required onChange={this.handleChange}/>
+        <button className="btn btn-lg btn-primary btn-block" type="submit" >Sign in</button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2020</p>
         </form>
         </div>
       );
--- server.js
+++ server.js
@@ -27,15 +27,15 @@ app.use(passport.session());
 require("./routes/api-routes.js")(app);
 
 
-
-
-
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
